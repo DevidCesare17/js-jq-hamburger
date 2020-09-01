4f051ef87315567365b9a1436e21a64c562d752b
Davide Cesare <davidereaper@gmail.com>
2020-09-01 18:16:55 +0200

    Refactoring pt.2

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // 1. Apertura menù (diventa display: block)
-$(".header-right a>i.fa-bars").click(
+$(".header-right > a").click(
   function () {
     $(".hamburger-menu").addClass("active");
   }
 );
 
 // 2. Chiusura menù (ritorno a display: none)
-$(".hamburger-menu a>i.fa-times").click(
+$(".close").click(
   function() {
     $(".hamburger-menu").removeClass("active");
   }
